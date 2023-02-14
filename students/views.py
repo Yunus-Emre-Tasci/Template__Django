046f50449d569cd27c7e419576c955a8e805d827
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-02-14 14:28:57 +0300

    context

--- students/views.py
+++ students/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,)
+    
+    context={
+        "title":"Clarusway",
+        "desc":"This is a description",
+        "number":"1111",
+        
+    }
+    
+    return render(request,"students/home.html",context)
